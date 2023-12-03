730469df6db5ae42496c3243f9f9093994e977db
Denys Mentiei <endden@gmail.com>
2023-12-03 15:19:26 +0100

    Updated boilerplate.

--- boilerplate.rs
+++ boilerplate.rs
@@ -15,6 +15,14 @@ fn main() -> Result<()> {
 	Ok(())
 }
 
+fn part1(input: &str) -> i32 {
+	todo!()
+}
+
+fn part2(input: &str) -> i32 {
+	todo!()
+}
+
 #[cfg(test)]
 mod tests {
 	use super::*;
@@ -23,9 +31,11 @@ mod tests {
 
 	#[test]
 	fn test_part1() {
+		assert_eq!(part1(INPUT), 0);
 	}
 
 	#[test]
 	fn test_part2() {
+		assert_eq!(part2(INPUT), 0);
 	}
 }
